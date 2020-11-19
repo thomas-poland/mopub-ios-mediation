0c664021b0382ff83b523578e020b8f0afd79f9c
chauduyphanvu1 <vchau@twitter.com>
2020-11-19 11:05:40 -0500

    Facebook - 6.2.0.1

--- FacebookAudienceNetwork/CHANGELOG.md
+++ FacebookAudienceNetwork/CHANGELOG.md
@@ -1,4 +1,7 @@
 ## Changelog
+* 6.2.0.1
+    * Add support for `fullscreenAdAdapterAdDidDismiss:` to signal that the fullscreen ad is closing and the state should be reset. To use this adapter version, you need v5.15.0 of the MoPub iOS SDK at the minimum.
+
 * 6.2.0.0
     * This version of the adapters has been certified with Facebook Audience Network 6.2.0 and MoPub 5.14.1.
 
--- FacebookAudienceNetwork/FacebookRewardedVideoCustomEvent.m
+++ FacebookAudienceNetwork/FacebookRewardedVideoCustomEvent.m
@@ -202,6 +202,12 @@
 {
     MPLogAdEvent([MPLogEvent adDidDisappearForAdapter:NSStringFromClass(self.class)], self.fbPlacementId);
     [self.delegate fullscreenAdAdapterAdDidDisappear:self];
+    
+    // Signal that the fullscreen ad is closing and the state should be reset.
+    // `fullscreenAdAdapterAdDidDismiss:` was introduced in MoPub SDK 5.15.0.
+    if ([self.delegate respondsToSelector:@selector(fullscreenAdAdapterAdDidDismiss:)]) {
+        [self.delegate fullscreenAdAdapterAdDidDismiss:self];
+    }
 }
 
 /*!
