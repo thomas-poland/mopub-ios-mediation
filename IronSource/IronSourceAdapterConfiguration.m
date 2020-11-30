0207c070d4cabe28032b105c08668d215ac13441
chauduyphanvu1 <vchau@twitter.com>
2020-11-30 16:20:42 -0500

    ironSource - 7.0.3.0.3

--- IronSource/IronSourceAdapterConfiguration.m
+++ IronSource/IronSourceAdapterConfiguration.m
@@ -32,7 +32,7 @@ NSString * const kIronSourceAppkey = @"applicationKey";
 #pragma mark - MPAdapterConfiguration
 
 - (NSString *)adapterVersion {
-    return @"7.0.3.0.2";
+    return @"7.0.3.0.3";
 }
 
 - (NSString *)biddingToken {
