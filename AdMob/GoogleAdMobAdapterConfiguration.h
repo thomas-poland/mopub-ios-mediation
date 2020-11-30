07df6ca553d1e4e9ec2bcd6278e60078c8a62320
chauduyphanvu1 <vchau@twitter.com>
2020-11-30 15:17:29 -0500

    Google - 7.68.0.2

--- AdMob/GoogleAdMobAdapterConfiguration.h
+++ AdMob/GoogleAdMobAdapterConfiguration.h
@@ -8,8 +8,8 @@
 #import <Foundation/Foundation.h>
 #if __has_include(<MoPub/MoPub.h>)
 #import <MoPub/MoPub.h>
-#elif __has_include(<MoPubSDKFramework/MoPub.h>)
-#import <MoPubSDKFramework/MoPub.h>
+#elif __has_include(<MoPubSDK/MoPub.h>)
+#import <MoPubSDK/MoPub.h>
 #else
 #import "MPBaseAdapterConfiguration.h"
 #import "MoPub.h"
