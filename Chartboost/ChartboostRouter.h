c5ea745eca00f15460faf8640e85e52f0fad8e1d
chauduyphanvu1 <vchau@twitter.com>
2020-11-30 15:55:49 -0500

    Chartboost - 8.3.1.1.2

--- Chartboost/ChartboostRouter.h
+++ Chartboost/ChartboostRouter.h
@@ -9,8 +9,8 @@
 
 #if __has_include(<MoPub/MoPub.h>)
     #import <MoPub/MoPub.h>
-#elif __has_include(<MoPubSDKFramework/MoPub.h>)
-    #import <MoPubSDKFramework/MoPub.h>
+#elif __has_include(<MoPubSDK/MoPub.h>)
+    #import <MoPubSDK/MoPub.h>
 #else
     #import "MoPub.h"
 #endif
