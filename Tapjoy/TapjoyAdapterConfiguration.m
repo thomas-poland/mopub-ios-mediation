7c6f75356d9e97a966b55cd75118e43e662e0ead
chauduyphanvu1 <vchau@twitter.com>
2020-11-30 16:59:09 -0500

    Tapjoy - 12.7.1.2

--- Tapjoy/TapjoyAdapterConfiguration.m
+++ Tapjoy/TapjoyAdapterConfiguration.m
@@ -41,7 +41,7 @@ typedef NS_ENUM(NSInteger, TapjoyAdapterErrorCode) {
 #pragma mark - MPAdapterConfiguration
 
 - (NSString *)adapterVersion {
-    return @"12.7.1.1";
+    return @"12.7.1.2";
 }
 
 - (NSString *)biddingToken {
