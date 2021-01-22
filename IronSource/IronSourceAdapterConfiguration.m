5be7e669ed786a4081ad95ae8e5a81411118b402
Hikmet Mert Kahraman <hmertkahraman@twitter.com>
2021-01-22 17:15:44 +0000

    ironSource - 7.1.0.0.0

--- IronSource/IronSourceAdapterConfiguration.m
+++ IronSource/IronSourceAdapterConfiguration.m
@@ -32,7 +32,7 @@ NSString * const kIronSourceAppkey = @"applicationKey";
 #pragma mark - MPAdapterConfiguration
 
 - (NSString *)adapterVersion {
-    return @"7.0.4.0.0";
+    return @"7.1.0.0.0";
 }
 
 - (NSString *)biddingToken {
